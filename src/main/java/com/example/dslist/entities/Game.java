e720c9c1543fcb1c1e66c115c9de5c1407930b42
Isaac Dias <isaacbogui98@gmail.com>
2023-05-09 14:38:39 -0300

    Game seed

--- src/main/java/com/example/dslist/entities/Game.java
+++ src/main/java/com/example/dslist/entities/Game.java
@@ -29,7 +29,13 @@ public class Game {
 	private String platforms;
 	private Double score;
 	private String imgUrl;
+	
+	//@Column(columnDefinition = "TEXT") para textos acima de 255 caracteres
+	
+	@Column(columnDefinition = "TEXT")
 	private String shortDescription;
+	
+	@Column(columnDefinition = "TEXT")
 	private String longDescription;
 	
 	public Game() {
