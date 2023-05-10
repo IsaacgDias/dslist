f777d6c5d6830108762d7ae0135dbccc3d14eceb
Isaac Dias <isaacbogui98@gmail.com>
2023-05-10 14:28:55 -0300

    get lists

---
+++ src/main/java/com/example/dslist/services/GameListService.java
@@ -0,0 +1,26 @@
+package com.example.dslist.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.example.dslist.dto.GameListDTO;
+import com.example.dslist.dto.GameMinDTO;
+import com.example.dslist.entities.GameList;
+import com.example.dslist.repositories.GameListRepository;
+
+@Component
+public class GameListService {
+	
+	@Autowired
+	private GameListRepository gamelistRepository;
+	
+	@Transactional(readOnly = true)
+	public List<GameListDTO> findAll() {
+		List<GameList> result = gamelistRepository.findAll();
+		return result.stream().map(x -> new GameListDTO(x)).toList();
+		
+	}
+}
