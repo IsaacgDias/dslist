f5796d7d549f6bd4ab25d49da6133f3aa64fa8e3
Isaac Dias <isaacbogui98@gmail.com>
2023-05-10 14:10:01 -0300

    Get por id

--- src/main/java/com/example/dslist/controllers/GameController.java
+++ src/main/java/com/example/dslist/controllers/GameController.java
@@ -4,9 +4,11 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.dslist.dto.GameDTO;
 import com.example.dslist.dto.GameMinDTO;
 import com.example.dslist.services.GameService;
 
@@ -22,6 +24,12 @@ public class GameController {
 			 List<GameMinDTO> result = gameServices.findAll();
 			 return result;
 		}
+		
+		@GetMapping(value = "/{id}")
+		public GameDTO findById(@PathVariable Long id) {
+			 GameDTO result = gameServices.findById(id);
+			 return result;
+		}	
 		
 	}
 	
